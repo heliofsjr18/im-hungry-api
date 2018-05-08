2110bcee4b4339ae8061283df92c0def16eb850e
Rafael Eduardo Lima <paracafa@gmail.com>
2018-05-08 20:55:37 -0300

    Fidelidade - Salvando na tabela empresa se tem fidelidade ou não.

--- DAO/FidelidadeFilialDAO.php
+++ DAO/FidelidadeFilialDAO.php
@@ -11,12 +11,18 @@ class FidelidadeFilialDAO
                 VALUES ( ?, ?, ?, ?);";
         $stmt = $conn->prepare($sql);
 
+        $sql1 = "update empresa_filial set filial_fidelidade = 1 where filial_id = ?";
+
         try {
             $stmt->bindValue(1,$fidelidadeFilial->getQtd(), PDO::PARAM_STR);
             $stmt->bindValue(2,$fidelidadeFilial->getValor(), PDO::PARAM_STR);
             $stmt->bindValue(3,$fidelidadeFilial->getBeneficio(), PDO::PARAM_STR);
             $stmt->bindValue(4,$fidelidadeFilial->getFilialId(), PDO::PARAM_STR);
             $stmt->execute();
+
+            $stmt1 = $conn->prepare($sql1);
+            $stmt1->bindValue(1,$fidelidadeFilial->getFilialId(), PDO::PARAM_STR);
+            $stmt1->execute();
 
             return array(
                 'status'    => 200,
@@ -41,9 +47,15 @@ class FidelidadeFilialDAO
         $sql = "delete from empresa_cartao_fid where cartao_fid_id = ?;";
         $stmt = $conn->prepare($sql);
 
+        $sql1 = "update empresa_filial set filial_fidelidade = 0 where filial_id = ?";
+
         try {
             $stmt->bindValue(1,$fidelidadeFilial->getId(), PDO::PARAM_STR);
             $stmt->execute();
+
+            $stmt1 = $conn->prepare($sql1);
+            $stmt1->bindValue(1,$fidelidadeFilial->getFilialId(), PDO::PARAM_STR);
+            $stmt1->execute();
 
             return array(
                 'status'    => 200,
